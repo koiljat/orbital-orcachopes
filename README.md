e516ba6a6e6bbbad2832524f79e653beb82ddf0e
koiljat <koiljat@u.nus.edu>
2023-05-12 15:30:26 +0800

    Added test handler

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ORCAchopes
 
 This is a an orbital project for a reservation system through a telegram bot.
+
+TOKEN = '6183521726:AAGqHZywmdqCp6JsoJnbi-AGbS4nRe31xyI'
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("This is a test")
+import telegram
+from telegram.ext import CommandHandler, Updater
+
+TOKEN = '6183521726:AAGqHZywmdqCp6JsoJnbi-AGbS4nRe31xyI'
+
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="This is a test")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def book(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Select booking")
+
+book_handler = CommandHandler('book', book)
+dispatcher.add_handler(book_handler)
+
+updater.start_polling()
