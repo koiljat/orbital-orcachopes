c76f2843e0c42dec7746c51f1db1278119e8982f
mavloh007 <104507877+mavloh007@users.noreply.github.com>
2023-05-16 18:41:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,10 @@ updater.start_polling()
 
 import mysql.connector
 
-
+connection = mysql.connector.connect(
+    host="localhost",
+    port=3306,  
+    user="root",
+    password="Nerfcs45&",
+    database="Orbital Datab"
+)
